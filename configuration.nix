612df3b94b4c67130da19e2cc598085bc1ecc33b
Emily Chwiggy <emilycwilke@gmail.com>
2024-01-10 23:41:51 +0100

    make nicer

--- configuration.nix
+++ configuration.nix
@@ -4,45 +4,35 @@
   imports = [ <nixpkgs/nixos/modules/installer/virtualbox-demo.nix> ];
 
   # Let demo build as a trusted user.
-nix.settings.trusted-users = [ "demo" ];
-nixpkgs = {
-config = {
-allowUnfree = true;
-};
-};
-# Mount a VirtualBox shared folder.
-# This is configurable in the VirtualBox menu at
-# Machine / Settings / Shared Folders.
-# fileSystems."/mnt" = {
-#   fsType = "vboxsf";
-#   device = "nameofdevicetomount";
-#   options = [ "rw" ];
-# };
+  nix.settings.trusted-users = [ "demo" ];
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
+  
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
 
-# By default, the NixOS VirtualBox demo image includes SDDM and Plasma.
-# If you prefer another desktop manager or display manager, you may want
-# to disable the default.
-services.xserver.desktopManager.plasma5.enable = true;
-services.xserver.displayManager.sddm.enable = true;
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "dvorak";
+    xkbOptions = "compose:ralt";
+  };
 
-services.xserver = {
-layout = "us";
-xkbVariant = "dvorak";
-xkbOptions = "compose:ralt";
-};
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
 
-# Set your time zone.
-time.timeZone = "Europe/Amsterdam";
+  # List packages installed in system profile. To search, run:
+  # \$ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    wget
+    vscode
+    firefox
+  ];
 
-# List packages installed in system profile. To search, run:
-# \$ nix search wget
- environment.systemPackages = with pkgs; [
-git
-   wget
-vscode
-firefox ];
-
-# Enable the OpenSSH daemon.
-# services.openssh.enable = true;
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
 }
