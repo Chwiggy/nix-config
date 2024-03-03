859cd951ebd0519a1b129cf32b7472b99d8de283
chwiggy <emilycwilke@gmail.com>
2024-03-03 20:46:00 +0100

    added flake

---
+++ home.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "lotte";
+  home.homeDirectory = "/home/lotte";
+
+  # user packages
+  home.packages = [
+      pkgs.firefox
+      pkgs.kate
+      pkgs.vscode
+
+      # messaging apps
+      pkgs.discord
+      pkgs.nheko
+      pkgs.signal-desktop
+
+      # entertainment
+      pkgs.spotify
+      pkgs.freetube
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
