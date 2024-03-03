8e8c0c025d44c168aa1b17d8293f9dd0c232d0dd
chwiggy <emilycwilke@gmail.com>
2024-03-03 21:20:09 +0100

    moved home-manager config

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.lotte = import ./home.nix;
+            home-manager.users.lotte = import ./home-manager/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
