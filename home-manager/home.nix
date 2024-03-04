f4ab8c09d3ebfaf26cc2282a82eb3f3fbfd2b6fc
chwiggy <emilycwilke@gmail.com>
2024-03-04 15:45:37 +0100

    added zsh

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -64,6 +64,18 @@
     ];
   };
 
+  # ZSH config
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases.ll = "ls -l";
+    history.size = 10000;
+    history.path = "$(config.xdg.dataHome)/zsh/history";
+  };
+
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
