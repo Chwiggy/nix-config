ca2f91adc883dde20999161e67503f436b96ddce
chwiggy <emilycwilke@gmail.com>
2024-03-06 22:25:44 +0100

    added configurationLimit

--- hosts/TGVSudEst/configuration.nix
+++ hosts/TGVSudEst/configuration.nix
@@ -13,6 +13,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 3;
 
   networking.hostName = "TGVSudEst"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
