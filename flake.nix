fdda6a0f9bcfe6f1f8ceb34e06ae7e48ea8e829a
chwiggy <emilycwilke@gmail.com>
2024-03-21 17:33:40 +0100

    meow

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
+      # I want more computers to install nix on
       TGVSudEst = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
