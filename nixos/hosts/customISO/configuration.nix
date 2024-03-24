1d78c33081bac57aea3547545e9f50d102354781
chwiggy <emilycwilke@gmail.com>
2024-03-24 17:27:12 +0100

    removed import

--- nixos/hosts/customISO/configuration.nix
+++ nixos/hosts/customISO/configuration.nix
@@ -8,7 +8,7 @@
   ...
 }: {
   imports = [
-    inputs.home-manager.nixosModules.home-manager
+    #inputs.home-manager.nixosModules.home-manager
 
     # Import shared settings
     ../../common
@@ -34,5 +34,16 @@
 
   networking.networkmanager.enable = true;
 
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+
+
+  
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
 
 }
