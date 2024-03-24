a0430a4a8cdbcf56b875e1f7a4258fe7636d9dcd
chwiggy <emilycwilke@gmail.com>
2024-03-24 17:21:33 +0100

    hrmm

--- flake.nix
+++ flake.nix
@@ -32,24 +32,24 @@
           }
         ];
       };
-      customISO = nixpkgs.lib.nixos {
-        system = "x86_64-linux";
-        modules = [
-          # ISO things
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-plasma6.nix"
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
-
-          ./nixos/hosts/customIso/configuration.nix
-          # ./common/
-          home.manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.sharedModules = [inputs.plasma-manager.homeManagerModules.plasma-manager];
-            home-manager.users.lotte = import ./homes/lotte.nix;
-          }
-        ];
-      };
+      #customISO = nixpkgs.lib.nixos {
+      #  system = "x86_64-linux";
+      #  modules = [
+      #    # ISO things
+      #    "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-plasma6.nix"
+      #    "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+#
+      #    ./nixos/hosts/customIso/configuration.nix
+      #    # ./common/
+      #    home.manager.nixosModules.home-manager
+      #    {
+      #      home-manager.useGlobalPkgs = true;
+      #      home-manager.useUserPackages = true;
+      #      home-manager.sharedModules = [inputs.plasma-manager.homeManagerModules.plasma-manager];
+      #      home-manager.users.lotte = import ./homes/lotte.nix;
+      #    }
+      #  ];
+      #};
     };
   };
 }
