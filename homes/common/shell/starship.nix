9a88cdcd9c3c8eba9e3fed2fb5242f00a1736043
Chwiggy <emiilycwilke@gmail.com>
2024-04-15 23:12:10 +0200

    added sudo

--- homes/common/shell/starship.nix
+++ homes/common/shell/starship.nix
@@ -1,10 +1,22 @@
 {
+  lib, ...
+}:{
     programs.starship = {
     enable = true;
     settings = {
       add_newline = true;
+      format = lib.concatStrings [
+          "$sudo"
+          "$all"
+        ];
       character.success_symbol = "[➜](bold purple)";
       package.disabled = true;
+      sudo = {
+          symbol = "witch ";
+          style = "bold blue";
+          format = "[$symbol]($style)";
+          disabled = false;
+      };
       hostname = {
         ssh_only = false;
         format = "[$hostname](bold yellow)[$ssh_symbol](bold blue):";
