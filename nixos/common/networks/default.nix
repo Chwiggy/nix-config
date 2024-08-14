322ac6417f292a088488dccb263d6529cf242981
Chwiggy <emilycwilke@gmail.com>
2024-08-14 13:38:25 +0200

    added RZL and eduroam configs

--- nixos/common/networks/default.nix
+++ nixos/common/networks/default.nix
@@ -28,6 +28,39 @@
         wep-key-flags = "1";
       };
     };
+    eduroam = {
+      "802-1x" = {
+        anonymous-identity = "eduroamHDcat2024@uni-heidelberg.de";
+        ca-cert = "/home/lotte/.config/cat_installer/ca.pem";
+        eap = "peap;";
+        identity = "dg254@uni-heidelberg.de";
+        password-flags = "1";
+        phase2-auth = "mschapv2";
+      };
+      connection = {
+        id = "eduroam";
+        interface-name = "wlp0s20f3";
+        permissions = "user:lotte:;";
+        type = "wifi";
+        uuid = "5978a2fd-435e-485a-97e4-2bbd96786ae5";
+      };
+      ipv4 = {
+        method = "auto";
+      };
+      ipv6 = {
+        addr-gen-mode = "stable-privacy";
+        method = "auto";
+      };
+      proxy = {};
+      wifi = {
+        mode = "infrastructure";
+        ssid = "eduroam";
+      };
+      wifi-security = {
+        auth-alg = "open";
+        key-mgmt = "wpa-eap";
+      };
+    };
     "eduroam (real)" = {
       connection = {
         id = "eduroam (real)";
@@ -46,6 +79,30 @@
       wifi = {
         mode = "infrastructure";
         ssid = "eduroam (real)";
+      };
+      wifi-security = {
+        key-mgmt = "wpa-psk";
+        psk-flags = "1";
+      };
+    };
+    RZL-hardcore = {
+      connection = {
+        id = "RZL-hardcore";
+        permissions = "user:lotte:;";
+        type = "wifi";
+        uuid = "f79dc8ea-783b-42c8-881e-4629b1e80652";
+      };
+      ipv4 = {
+        method = "auto";
+      };
+      ipv6 = {
+        addr-gen-mode = "stable-privacy";
+        method = "auto";
+      };
+      proxy = {};
+      wifi = {
+        mode = "infrastructure";
+        ssid = "RZL-hardcore";
       };
       wifi-security = {
         key-mgmt = "wpa-psk";
