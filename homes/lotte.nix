ffa0248f132c4e88f0e27c1a9a23ebc7d26fa36e
Chwiggy <emilycwilke@gmail.com>
2024-08-27 15:09:31 +0200

    fix vscode extensions

--- homes/lotte.nix
+++ homes/lotte.nix
@@ -118,18 +118,21 @@
       enable = true;
       package = pkgs.vscode;
       extensions = with pkgs.vscode-extensions; [
-        jnoortheen.nix-ide
         arrterian.nix-env-selector
-        mkhl.direnv
-        ms-vscode-remote.remote-ssh
-        rust-lang.rust-analyzer
-        ms-toolsai.jupyter
-        nvarner.typst-lsp
-        tomoki1207.pdf
-        kamadorueda.alejandra
         catppuccin.catppuccin-vsc
         catppuccin.catppuccin-vsc-icons
+        charliermarsh.ruff
+        jnoortheen.nix-ide
+        kamadorueda.alejandra
         mechatroner.rainbow-csv
+        mkhl.direnv
+        ms-python.python
+        ms-python.debugpy
+        ms-toolsai.jupyter
+        ms-vscode-remote.remote-ssh
+        nvarner.typst-lsp
+        rust-lang.rust-analyzer
+        tomoki1207.pdf
       ];
     };
   };
