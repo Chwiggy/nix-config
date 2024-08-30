bc5afce78aeccced3259d559ca0fb27a51721bb7
Chwiggy <emilycwilke@gmail.com>
2024-08-30 15:46:53 +0200

    set explicit powerdevil config to avoid plasma-manager bug

--- homes/common/plasma-desktop/default.nix
+++ homes/common/plasma-desktop/default.nix
@@ -9,4 +9,22 @@
   imports = [
     ./plasma-manager.nix
   ];
+
+  programs.plasma.powerdevil = {
+    AC = {
+      autoSuspend.action = "sleep";
+      whenLaptopLidClosed = "sleep";
+      whenSleepingEnter = "standbyThenHibernate";
+    };
+    battery = {
+      autoSuspend.action = "sleep";
+      whenLaptopLidClosed = "hibernate";
+      whenSleepingEnter = "standbyThenHibernate";
+    };
+    lowBattery = {
+      autoSuspend.action = "sleep";
+      whenLaptopLidClosed = "hibernate";
+      whenSleepingEnter = "standbyThenHibernate";
+    };
+  };
 }
