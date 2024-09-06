e72402d36fe9a555347ba1b1a604eb3b6e6504af
Chwiggy <emilycwilke@gmail.com>
2024-09-06 19:34:33 +0200

    fix: remove profile permissions from network config

--- nixos/common/networks/default.nix
+++ nixos/common/networks/default.nix
@@ -4,7 +4,6 @@
       connection = {
         id = "Keks";
         interface-name = "wlp0s20f3";
-        permissions = "user:lotte:;";
         type = "wifi";
         uuid = "6c93d505-a7f3-4260-8c74-27b0f0a29863";
       };
@@ -40,7 +39,6 @@
       connection = {
         id = "eduroam";
         interface-name = "wlp0s20f3";
-        permissions = "user:lotte:;";
         type = "wifi";
         uuid = "5978a2fd-435e-485a-97e4-2bbd96786ae5";
       };
@@ -64,7 +62,6 @@
     "eduroam (real)" = {
       connection = {
         id = "eduroam (real)";
-        permissions = "user:lotte:;";
         type = "wifi";
         uuid = "8303387c-9535-40ac-be73-4baca7f29e38";
       };
@@ -88,7 +85,6 @@
     RZL-hardcore = {
       connection = {
         id = "RZL-hardcore";
-        permissions = "user:lotte:;";
         type = "wifi";
         uuid = "f79dc8ea-783b-42c8-881e-4629b1e80652";
       };
