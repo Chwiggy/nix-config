742d96873a94ab48b1a1fc087d39e28fd23c50f8
Chwiggy <emilycwilke@gmail.com>
2024-09-09 12:21:34 +0200

    feat: add .envrc to global gitignore for emily

--- homes/emily.nix
+++ homes/emily.nix
@@ -49,6 +49,8 @@
     ];
   };
 
+  programs.git.ignores = ["*.envrc"];
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
