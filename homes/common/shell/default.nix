86c38058cb783c176b77026da04ca5413d04fc29
Chwiggy <emilycwilke@gmail.com>
2025-02-15 19:09:07 +0100

    refactor: move out kitty config

--- homes/common/shell/default.nix
+++ homes/common/shell/default.nix
@@ -1,26 +1,9 @@
-{pkgs, ...}: {
+{...}: {
   imports = [
     ./zsh.nix
     ./starship.nix
     ./helix.nix
     ./zellij.nix
+    ./kitty.nix
   ];
-
-  programs.kitty = {
-    enable = true;
-    themeFile = "Catppuccin-Mocha";
-    font = {
-      name = "Atkinson Monolegible";
-      package = pkgs.atkinson-monolegible;
-      size = 11;
-    };
-    shellIntegration.enableZshIntegration = true;
-    settings = {
-      enable_audio_bell = true;
-      visual_bell_duration = 0.4;
-      bell_on_tab = "🔔 ";
-      confirm_os_window_close = 0;
-      wayland_enable_ime = false;
-    };
-  };
 }
---
+++ homes/common/shell/kitty.nix
@@ -0,0 +1,19 @@
+{pkgs, ...}: {
+  programs.kitty = {
+    enable = true;
+    themeFile = "Catppuccin-Mocha";
+    font = {
+      name = "Atkinson Monolegible";
+      package = pkgs.atkinson-monolegible;
+      size = 11;
+    };
+    shellIntegration.enableZshIntegration = true;
+    settings = {
+      enable_audio_bell = true;
+      visual_bell_duration = 0.4;
+      bell_on_tab = "🔔 ";
+      confirm_os_window_close = 0;
+      wayland_enable_ime = false;
+    };
+  };
+}
