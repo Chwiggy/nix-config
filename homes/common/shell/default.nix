dcdbfc1041930e20b3b15e563a04d52632601bee
Chwiggy <emilycwilke@gmail.com>
2025-02-15 18:40:02 +0100

    feat: update kitty config

--- homes/common/shell/default.nix
+++ homes/common/shell/default.nix
@@ -1,11 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  inputs,
-  outputs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     ./zsh.nix
     ./starship.nix
@@ -16,8 +9,18 @@
   programs.kitty = {
     enable = true;
     themeFile = "Catppuccin-Mocha";
-    font.name = "Atkinson Monolegible";
-    font.package = pkgs.atkinson-monolegible;
+    font = {
+      name = "Atkinson Monolegible";
+      package = pkgs.atkinson-monolegible;
+      size = 11;
+    };
     shellIntegration.enableZshIntegration = true;
+    settings = {
+      enable_audio_bell = true;
+      visual_bell_duration = 0.4;
+      bell_on_tab = "🔔 ";
+      confirm_os_window_close = 0;
+      wayland_enable_ime = false;
+    };
   };
 }
