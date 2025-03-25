13bb4a170afc467d6835004934cd4b7eb6190ffc
Emily C. Wilke <Emily.Wilke@heigit.org>
2025-03-25 09:33:45 +0100

    refactor: move zellij config to zellij.nix and remove zellij.kdl

--- homes/common/shell/zellij.nix
+++ homes/common/shell/zellij.nix
@@ -2,14 +2,24 @@
   programs.zellij = {
     # This autostarts zellij on zsh start
     # TODO find specific settings for vscode to avoid
-    #enable = true;
+    enable = true;
     enableZshIntegration = true;
-  };
-
-  home.file = {
-    "zellij" = {
-      source = ./zellij.kdl;
-      target = ".config/zellij/config.kdl";
+    settings = {
+      theme = "catppuccin-macchiato";
+      themes.catppuccin-macchiato = {
+        bg = "#5b6078"; # Surface2
+        fg = "#cad3f5";
+        red = "#ed8796";
+        green = "#a6da95";
+        blue = "#8aadf4";
+        yellow = "#eed49f";
+        magenta = "#f5bde6"; # Pink
+        orange = "#f5a97f"; # Peach
+        cyan = "#91d7e3"; # Sky
+        black = "#1e2030"; # Mantle
+        white = "#cad3f5";
+      };
+      show_startup_tips = false;
     };
   };
 }
