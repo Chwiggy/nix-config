09c72bdeae0ed7c9fa3a5737a535dfeae3aaad44
Chwiggy <emilycwilke@gmail.com>
2025-03-28 01:09:41 +0100

    feat: update kitty theme and make terminal bell a meow

--- homes/common/shell/kitty.nix
+++ homes/common/shell/kitty.nix
@@ -1,7 +1,9 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  meowLocation = ".config/kitty/meow.wav";
+in {
   programs.kitty = {
     enable = true;
-    themeFile = "Catppuccin-Mocha";
+    themeFile = "Catppuccin-Macchiato";
     font = {
       name = "Atkinson Monolegible";
       package = pkgs.atkinson-hyperlegible-mono;
@@ -14,6 +16,12 @@
       bell_on_tab = "🔔 ";
       confirm_os_window_close = 0;
       wayland_enable_ime = false;
+      bell_path = "~/${meowLocation}";
     };
+  };
+
+  home.file."meow" = {
+    source = ./meow.wav;
+    target = meowLocation;
   };
 }
