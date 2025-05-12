bc40e7bb2877accb1583adf8c7cbb322a13392b8
Emily C. Wilke <Emily.Wilke@heigit.org>
2025-05-12 09:22:18 +0200

    update: 2025-05-12

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
 
     # Lix
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0-2.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
