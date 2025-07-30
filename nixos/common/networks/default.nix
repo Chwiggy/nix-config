f6a120ff0a260eb2f0dba889b52dbf18791b0429
Emily C. Wilke <Emily.Wilke@heigit.org>
2025-07-30 08:39:59 +0200

    refactor: remove unused network

--- nixos/common/networks/default.nix
+++ nixos/common/networks/default.nix
@@ -3,33 +3,6 @@
     ./wifionice.nix
   ];
   networking.networkmanager.ensureProfiles.profiles = {
-    Keks = {
-      connection = {
-        id = "Keks";
-        interface-name = "wlp0s20f3";
-        type = "wifi";
-        uuid = "6c93d505-a7f3-4260-8c74-27b0f0a29863";
-      };
-      ipv4 = {
-        method = "auto";
-      };
-      ipv6 = {
-        addr-gen-mode = "default";
-        method = "auto";
-      };
-      proxy = {};
-      wifi = {
-        mode = "infrastructure";
-        ssid = "Keks";
-      };
-      wifi-security = {
-        auth-alg = "open";
-        key-mgmt = "wpa-psk";
-        leap-password-flags = "1";
-        psk-flags = "1";
-        wep-key-flags = "1";
-      };
-    };
     "eduroam (real)" = {
       connection = {
         id = "eduroam (real)";
